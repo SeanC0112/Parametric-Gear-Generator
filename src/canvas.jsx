87d4323462a3ed91c82dd2485fa51a63222f3d99
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-05-14 09:37:54 -0700

    change size if changed

--- src/canvas.jsx
+++ src/canvas.jsx
@@ -1,16 +1,33 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const Canvas = (props) => {
   const canvasRef = useRef(null);
 
-  const { draw, width, height } = props;
+  const [parentWidth, setParentWidth] = useState(0);
+  const [parentHeight, setParentHeight] = useState(0);
+
+  const { draw } = props;
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
-    canvas.width = width;
-    canvas.height = height;
+    // canvas.width = width;
+    // canvas.height = height;
+
+    const resizeIfNeeded = () => {
+      const parent = canvas.parentElement;
+      if (parent) {
+        const newWidth = parent.clientWidth;
+        const newHeight = parent.clientHeight;
+        if (newWidth !== parentWidth || newHeight !== parentHeight) {
+          setParentWidth(newWidth);
+          setParentHeight(newHeight);
+          canvas.width = newWidth;
+          canvas.height = newHeight;
+        }
+      }
+    };
 
     const render = () => {
       draw(context, canvas);
@@ -21,9 +38,9 @@ const Canvas = (props) => {
     return () => {
       window.cancelAnimationFrame(render);
     };
-  }, [draw, width, height]);
+  }, [draw]);
 
-  return <canvas ref={canvasRef} {...props} />;
+  return <canvas ref={canvasRef} />;
 };
 
 export default Canvas;
