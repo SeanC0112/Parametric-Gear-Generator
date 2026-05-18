32482a56699069e0a021b4cb264638e1675af225
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-05-18 14:57:14 -0700

    fix not declaring variables

---
+++ src/util.js
@@ -0,0 +1,31 @@
+export function generateGearsFromPath(path, baseRadius, toothScalar) {
+  let xPath = [];
+  let yPath = [];
+
+  let length = path.length;
+
+  let minX = Math.min(...path.map((point) => point.x));
+  let minY = Math.min(...path.map((point) => point.y));
+
+  path.forEach((point, index) => {
+    xPath.push({
+      x:
+        (baseRadius + (point.x - minX) * toothScalar) *
+        Math.cos((index / length) * 2 * Math.PI),
+      y:
+        (baseRadius + (point.x - minX) * toothScalar) *
+        Math.sin((index / length) * 2 * Math.PI),
+    });
+
+    yPath.push({
+      x:
+        (baseRadius + (point.y - minY) * toothScalar) *
+        Math.cos((index / length) * 2 * Math.PI),
+      y:
+        (baseRadius + (point.y - minY) * toothScalar) *
+        Math.sin((index / length) * 2 * Math.PI),
+    });
+  });
+
+  return { xPath, yPath };
+}
